a3f3556987729469ee73451416f860528af95eb4
Kumaresan BK <EVOLVUS\kumaresanb@ELPBLR-035.evolvus.com>
2018-12-27 19:12:33 +0530

    Rest APIS

---
+++ src/main/java/com/evolvus/springboot/curd/domain/Note.java
@@ -0,0 +1,52 @@
+package com.evolvus.springboot.curd.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="notes")
+@Data
+@Getter
+@Setter
+public class Note implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 983701926757186405L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="ID")
+	private Long id;
+	
+	@Column(name="title",nullable=false)
+	private String title;
+	
+	@Column(name="content",nullable=false)
+	private String content;
+	
+	@Column(name="create_date")
+	private Date createDate;
+	
+	@Column(name="updated_date")
+	private Date updatedDate;
+
+	@Override
+	public String toString() {
+		return "Note [id=" + id + ", title=" + title + ", content=" + content + ", createDate=" + createDate
+				+ ", updatedDate=" + updatedDate + "]";
+	}
+
+}
--- src/main/java/com/evolvus/springboot/curd/springbootcurd/SpringBootCurdApplication.java
+++ src/main/java/com/evolvus/springboot/curd/springbootcurd/SpringBootCurdApplication.java
@@ -2,8 +2,16 @@ package com.evolvus.springboot.curd.springbootcurd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableJpaRepositories(basePackages="com.evolvus.springboot.curd.repository")
+@EntityScan("com.evolvus.springboot")
+@ComponentScan("com.evolvus.springboot")
 public class SpringBootCurdApplication {
 
 	public static void main(String[] args) {
