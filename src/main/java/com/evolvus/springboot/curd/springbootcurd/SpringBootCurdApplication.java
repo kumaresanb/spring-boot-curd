05f401834bb7948635ffe8e0b3b09e8c01879d32
Kumaresan BK <EVOLVUS\kumaresanb@ELPBLR-035.evolvus.com>
2018-12-27 17:01:16 +0530

    first commit

---
+++ src/main/java/com/evolvus/springboot/curd/springbootcurd/SpringBootCurdApplication.java
@@ -0,0 +1,14 @@
+package com.evolvus.springboot.curd.springbootcurd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCurdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCurdApplication.class, args);
+	}
+
+}
+
